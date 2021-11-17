46a0f8cda50637fc0097ad060b2faef45cf76cb0
juliarose <julialunarose@gmail.com>
2021-11-17 04:20:13 -0500

    updated with flexible slice size

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ extern "C" {
 }
 
 // to accept the slice, rather than the pointer
-fn quicksort_wrapped(n: &mut [i32; 5], first: i32, last: i32) {
+fn quicksort_wrapped(n: &mut [i32], first: i32, last: i32) {
     unsafe {
         quicksort(n.as_mut_ptr(), first, last);
     }
 }
 
 fn main() {
-    let mut numbers_slice = [4, 2, 5, 3, 1];
+    let mut numbers_slice = [6, 4, 7, 2, 5, 9, 3, 1, 8];
     let first = 0;
     let last: i32 = (numbers_slice.len() as i32) - 1;
     
